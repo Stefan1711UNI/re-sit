c2a97956bd39dfed10a671a84e5f82cb502177cf
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-01-23 21:25:50 +0100

    data page start

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
